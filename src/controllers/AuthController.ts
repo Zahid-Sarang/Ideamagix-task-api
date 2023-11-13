4648d7c35a0b665f64b3ff7697767e4cdd71fd15
zahid-soft <zahidsarang95@gmail.com>
2023-11-13 16:43:20 +0530

    hashed the password

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -4,24 +4,28 @@ import { UserService } from "../services/UserServices";
 import { RegisterUserRequest } from "../types";
 
 export class AuthController {
-  constructor(private userService: UserService) {}
+    constructor(private userService: UserService) {}
 
-  async register(req: RegisterUserRequest, res: Response, next: NextFunction) {
-    const validationError = validationResult(req);
-    if (!validationError.isEmpty()) {
-      return res.status(400).json({ error: validationError.array() });
+    async register(
+        req: RegisterUserRequest,
+        res: Response,
+        next: NextFunction,
+    ) {
+        const validationError = validationResult(req);
+        if (!validationError.isEmpty()) {
+            return res.status(400).json({ error: validationError.array() });
+        }
+        const { userName, email, password } = req.body;
+        try {
+            const users = await this.userService.create({
+                userName,
+                email,
+                password,
+                role: "admin",
+            });
+            res.status(201).json({ users });
+        } catch (error) {
+            next(error);
+        }
     }
-    const { userName, email, password } = req.body;
-    try {
-      const users = await this.userService.create({
-        userName,
-        email,
-        password,
-        role: "admin",
-      });
-      res.status(201).json(users);
-    } catch (error) {
-      next(error);
-    }
-  }
 }
--- src/services/UserServices.ts
+++ src/services/UserServices.ts
@@ -1,19 +1,30 @@
 import createHttpError from "http-errors";
+import bcrypt from "bcrypt";
 import User from "../models/UserSchema";
 import { UserData } from "../types";
 export class UserService {
-  constructor() {}
-  async create({ userName, password, email, role }: UserData) {
-    try {
-      const isEmailExsit = await User.findOne({ email: email });
-      if (isEmailExsit) {
-        const err = createHttpError(400, "Email is already exists!");
-        throw err;
-      }
-      await User.create({ userName, email, password, role: role });
-    } catch (err) {
-      const error = createHttpError(500, "failed to create user");
-      throw error;
+    constructor() {}
+    async create({ userName, password, email, role }: UserData) {
+        // check if user already exists
+        const isEmailExsit = await User.findOne({ email: email });
+        if (isEmailExsit) {
+            const err = createHttpError(400, "Email is already exists!");
+            throw err;
+        }
+
+        // Hashed the password
+        const saltRounds = 10;
+        const hashedPassword = await bcrypt.hash(password, saltRounds);
+        try {
+            return await User.create({
+                userName,
+                email,
+                password: hashedPassword,
+                role: role,
+            });
+        } catch (err) {
+            const error = createHttpError(500, "failed to create user");
+            throw error;
+        }
     }
-  }
 }
